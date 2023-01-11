fcb6c653dd8967f06de78b74c505652cbe05527e
Matt Coffyn <matt.coffyn@gmail.com>
2023-01-11 18:21:26 +0000

    player tweaks

--- components/ Header.js
+++ components/ Header.js
@@ -6,7 +6,6 @@ const Header = () => (
   <header className="title-header">
     <h1 className={headingFont.className}>Matt Coffyn</h1>
     <div className="header-info">
-      <h2>Composer & Sound Designer</h2>
       <a href="mailto:matt@mattcoffyn.com">matt@mattcoffyn.com</a>
     </div>
     {/* <nav className={`nav ${headingFont.className}`}>
